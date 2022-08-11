79e4bae315e61b30a9b39f5b3a8a0677735c0e53
HolaSoyLuis <luis95velasquez@outlook.com>
2022-08-11 00:46:50 -0600

    seventh/chess/Chess.java Is working and now shows available movements in a selected position

---
+++ seventh/chess/ArrayExample.java
@@ -0,0 +1,9 @@
+public class ArrayExample{
+    public static void main(String[] args){
+        int[] elements = new int[2];
+        elements[0] = 0;
+        elements[1] = 1;
+        System.out.println(elements[0]);
+        System.out.println(elements[1]);
+    }
+}
--- seventh/chess/Chess.java
+++ seventh/chess/Chess.java
@@ -2,13 +2,18 @@ import java.util.*;
 
 public class Chess{
     public static void main(String[] args){
-        int horizontal = new int[8];
-        int vertical = new int[8];
+        int[] horizontal = new int[8];
+        int[] vertical = new int[8];
         
-        int currentRow = 0;
-        int currentColumn = 0;
+        int INITIAL_ROW = 0;
+        int INITIAL_COLUMN = 0;
+        
+        int currentRow = INITIAL_ROW;
+        int currentColumn = INITIAL_COLUMN;
 
         int movementCounter = 0;
+
+        Random rand = new Random();
         
         horizontal[0] = 2;
         horizontal[1] = 1;
@@ -29,32 +34,43 @@ public class Chess{
         vertical[7] = 1;
 
         // board
-        boolean[] board = new boolean[8][8];
+        boolean[][] board = new boolean[8][8];
 
-        if(!isOutOfBounds(currentRow, currentColumn, rowMovement, columnMovement)){
-            currentRow += vertical[movementCounter];
-            currentColumn += horizontal[movementCounter];
-        }
+        System.out.println("Current position (x, y): " + currentRow + ", " + currentColumn);
+        System.out.println();
+        
+        List<Integer> availableMovements = new ArrayList<Integer>();
+        availableMovements = getAvailableMovements(board, currentRow, currentColumn, horizontal, vertical);
+        
+        for(int i = 0; i < availableMovements.size(); i++)
+            System.out.println(availableMovements.get(i));
 
-        System.out.println(currentRow + ", " + currentColumn);
+        // if(isInBounds(currentRow, currentColumn, rowMovement, columnMovement)){
+        //     currentRow += vertical[movementCounter];
+        //     currentColumn += horizontal[movementCounter];
+        // }
+
     }
 
-    public static void IsOutOfBounds(int currentRow, int currentColumn, int rowMovement, int columnMovement){
-        boolean isXOutOfBounds = false;
-        boolean isYOutOfBounds = false;
-        if((currentRow + rowMovement) > 0 && (currentRow + rowMovement) < 8) isXOutOfBounds = true;
-        if((currentColumn + columnMovement) > 0 && (currentColumn + columnMovement) < 8) isYOutOfBounds = true;
-        if(isXOutOfBounds || isYOutOfBounds) return true;
+    public static boolean isInBounds(int currentRow, int currentColumn, int rowMovement, int columnMovement){
+        boolean isXInBounds = false;
+        boolean isYInBounds = false;
+        int rowNextMovement = currentRow + rowMovement;
+        int columnNextMovement = currentColumn + columnMovement;
+        if(rowNextMovement > 0 && rowNextMovement < 8) isXInBounds = true;
+        if(columnNextMovement > 0 && columnNextMovement < 8) isYInBounds = true;
+        if(isXInBounds && isYInBounds) return true;
         return false;
     }
 
     public static List<Integer> getAvailableMovements(boolean[][] board, int currentRow, int currentColumn, int[] horizontal, int[] vertical){
         List<Integer> availableMovements = new ArrayList<Integer>();
         for(int i = 0; i < 8; i++){
-            currentRow += vertical[i];
-            currentColumn += horizontal[i];
-            if(!board[currentRow][currentColumn])
-                availableMovements.add(i);
+            int rowMovement = currentRow + vertical[i];
+            int columnMovement = currentColumn +  horizontal[i];
+            if(isInBounds(currentRow, currentColumn, rowMovement, columnMovement))
+                if(!board[rowMovement][columnMovement])
+                    availableMovements.add(i);
         }
         return availableMovements;
     }
